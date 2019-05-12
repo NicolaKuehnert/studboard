db94f373f7a1bbe54c397369f203b4381fc6085d
Nico Ohlemeyer <nico.ohlemeyer@posteo.de>
2019-05-12 11:24:49 +0200

    1. Array ausgabe

--- src/Praktikum Dateien/script.js
+++ src/Praktikum Dateien/script.js
@@ -3,3 +3,8 @@ var translate_projekt = new Array("Projekt","project");
 var translate_menue = new Array("Menü, menu");
 var translate_kurzbeschreibung = ("Kurzbeschreibung", "short description");
 var translate_langbeschreibung = ("Langbeschreibung", "long description");
+console.log(translate_projekt);
+console.log(translate_menue);
+console.log(translate_kurzbeschreibung);
+console.log(translate_langbeschreibung);
+
