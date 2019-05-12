12ab45bfc2b002476c25dce4f57b2de58949491a
Nico Ohlemeyer <nico.ohlemeyer@posteo.de>
2019-05-12 11:32:54 +0200

    1. Schreibfehler fix

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 /* Aufgabe 1: Zuordnung von Begriffen über Array */
 var translate_projekt = new Array("Projekt","project");
-var translate_menue = new Array("Menü, menu");
-var translate_kurzbeschreibung = ("Kurzbeschreibung", "short description");
-var translate_langbeschreibung = ("Langbeschreibung", "long description");
+var translate_menue = new Array("Menü", "menu");
+var translate_kurzbeschreibung = new Array("Kurzbeschreibung", "short description");
+var translate_langbeschreibung = new Array("Langbeschreibung", "long description");
 console.log(translate_projekt);
 console.log(translate_menue);
 console.log(translate_kurzbeschreibung);
