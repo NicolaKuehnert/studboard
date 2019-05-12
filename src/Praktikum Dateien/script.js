7e132e92bc657d9f7b930ab4afa3bf964399886e
Nico Ohlemeyer <nico.ohlemeyer@posteo.de>
2019-05-12 11:21:18 +0200

    1. Array für Deutsch/Englische Begriffe angelegt

---
+++ src/Praktikum Dateien/script.js
@@ -0,0 +1,5 @@
+/* Aufgabe 1: Zuordnung von Begriffen über Array */
+var translate_projekt = new Array("Projekt","project");
+var translate_menue = new Array("Menü, menu");
+var translate_kurzbeschreibung = ("Kurzbeschreibung", "short description");
+var translate_langbeschreibung = ("Langbeschreibung", "long description");
