1ee37a351887822d75e583e3f621ae5853be3978
Nico Ohlemeyer <nico.ohlemeyer@posteo.de>
2019-05-12 12:16:32 +0200

    2a

--- src/script.js
+++ src/script.js
@@ -1,10 +1,32 @@
 /* Aufgabe 1: Zuordnung von Begriffen über Array */
-var translate_projekt = new Array("Projekt","project");
-var translate_menue = new Array("Menü", "menu");
-var translate_kurzbeschreibung = new Array("Kurzbeschreibung", "short description");
-var translate_langbeschreibung = new Array("Langbeschreibung", "long description");
+let translate_projekt = new Array("Projekt","project");
+let translate_menue = new Array("Menü", "menu");
+let translate_kurzbeschreibung = new Array("Kurzbeschreibung", "short description");
+let translate_langbeschreibung = new Array("Langbeschreibung", "long description");
 console.log(translate_projekt);
 console.log(translate_menue);
 console.log(translate_kurzbeschreibung);
 console.log(translate_langbeschreibung);
 
+/*Aufgabe 2: Abstrakte Klasse */
+class Zuordnung{
+    constructor(param1){
+        this.attribut1 = param1;
+        this.attribut2 = param2;
+        this.attribut3 = param3;
+    }
+
+    methode1(){
+        return this.attribut1;
+    }
+
+    methode2(){
+        return this.attribut2;
+    }
+
+    methode3(){
+        return this.attribut3;
+    }
+}
+zuordnung1 = new Zuordnung("user1", "deutsch", "Projekt42");
+zuordnung2 = new Zuordnung("user2", "englisch", "science project 0815");
