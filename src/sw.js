e416eddc6513d303ea92e181f17153c2469aaa06
Nico Ohlemeyer <nico.ohlemeyer@posteo.de>
2019-05-19 14:22:15 +0200

    Worker hinzugefügt

---
+++ src/sw.js
@@ -0,0 +1,6 @@
+/* Alle Seiten cachen */
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => cachedResponse || fetch(event.request))
+);
+});
