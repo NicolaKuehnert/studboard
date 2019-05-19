a9162db8632bfeff594edf36438fb502d8a84ff2
Nico Ohlemeyer <nico.ohlemeyer@posteo.de>
2019-05-19 15:09:27 +0200

    Worker hinzugefügt mit Workbox Framework https://developers.google.com/web/tools/workbox/guides/get-started

--- src/initworker.js
+++
@@ -1,7 +0,0 @@
-/* unterstützt Browser überhaupt Service Worker? Falls ja, registrieren. */
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-        .then(registration => console.log('Service worker successfully registered for ${registration.scope}'))
-        .catch(err => console.log('Service worker registration failed: ${err}'));
-}
---
+++ src/service-worker.js
@@ -0,0 +1,7 @@
+console.log('Hello from service-worker.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
