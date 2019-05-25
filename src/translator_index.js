10b2c3f0986581527bddeb5e678bc963aa78f3ed
Nico Ohlemeyer <nico.ohlemeyer@posteo.de>
2019-05-26 01:22:18 +0200

    Übersetzungs-Javascript für die projektuebersicht hinzugefügt Übersetzungs-Javascript für index.html (update)

--- src/translator_index.js
+++ src/translator_index.js
@@ -2,8 +2,7 @@
 
 window.onload = function()
 {
-    console.log("Ausgabe: ");
-    console.log(document.body.children[1].children[0].children[0].children[1].innerHTML);
     document.body.children[1].children[0].children[0].children[1].innerHTML = "Project Overview";
     document.body.children[1].children[0].children[0].children[2].innerHTML = "New Project";
+
 }
---
+++ src/translator_projektuebersicht.js
@@ -0,0 +1,11 @@
+//Dieses Script fügt in projektuebersicht.html englische Begriffe ein
+
+window.onload = function()
+{
+    document.body.children[1].children[0].children[0].children[1].innerHTML = "Project Overview";
+    document.body.children[1].children[0].children[0].children[2].innerHTML = "New Project";
+    document.body.children[1].children[0].children[1].children[0].innerHTML = "Content";
+    document.body.children[1].children[0].children[1].children[1].children[0].innerHTML = "Project 1";
+    document.body.children[1].children[0].children[1].children[2].children[0].innerHTML = "Project 2";
+
+}
