a75f08e7f5607d5c15bb0c1fc75aad03ef9c0fb1
NicolaKuehnert <nicola.kuehnert@outlook.de>
2019-05-27 19:05:31 +0200

    projekte laden

--- src/converter.js
+++ src/converter.js
@@ -56,11 +56,52 @@ u1 = new User(
     1);
 
 p1 = new Project(
-    "Super Titel",
+    "Projekt 1",
     "25.05.2019",
     "30.05.2019",
-    "Ein super Test-Projekt",
-    "Mit gaaaannnzz langer Beschreibung",
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    "<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>",
     "binär-wert-bild",
     "Müller",
     {ziel:"Lauffähig"},
@@ -68,11 +109,52 @@ p1 = new Project(
 );
 
 p2 = new Project(
-    "Super Projekt",
+    "Projekt 2",
     "26.05.2019",
     "30.05.2019",
-    "Ein super Test-Projekt",
-    "Mit gaaaannnzz langer Beschreibung",
+    "tema tis rolod muspi meroL tse sutcnas atamikat aes on ,nergrebug dsak atilc tetS .muber ae te serolod oud otsuj te masucca te soe orev tA",
+    "<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>\n" +
+    "                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+    "                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo\n" +
+    "                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor\n" +
+    "                        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor\n" +
+    "                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et\n" +
+    "                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem\n" +
+    "                        ipsum dolor sit amet.</p>",
     "binär-wert-bild",
     "Müller",
     {ziel:"Lauffähig"},
--- src/translator_projektuebersicht.js
+++ src/translator_projektuebersicht.js
@@ -1,11 +1,63 @@
 //Dieses Script fügt in projektuebersicht.html englische Begriffe ein
+let german = {
+    project_overview:"Projektübersicht",
+    new_project:"Neues Projekt",
+    content:"Inhalt",
+    project:"Projekt"
+};
+
+let english = {
+    project_overview:"Project Overview",
+    new_project:"New Project",
+    content:"Content",
+    project:"Project"
+};
+
+title ="<section>\n" +
+    "                <article>\n" +
+    "                    <header>\n" +
+    "                        <h3>";
+
+body ="</h3>\n" +
+    "                    </header>\n" +
+    "                    <div style=\"width: 90%\">\n" +
+    "                        <p>";
+
+link="</p>\n" +
+    "                    </div>\n" +
+    "                    <footer><a href=\"";
+
+end="\">Footer</a></footer>\n" +
+    "\n" +
+    "\n" +
+    "                </article>\n" +
+    "            </section>";
+
+var lang = navigator.language || navigator.userLanguage;
 
 window.onload = function()
 {
-    document.body.children[1].children[0].children[0].children[1].innerHTML = "Project Overview";
-    document.body.children[1].children[0].children[0].children[2].innerHTML = "New Project";
-    document.body.children[1].children[0].children[1].children[0].innerHTML = "Content";
-    document.body.children[1].children[0].children[1].children[1].children[0].innerHTML = "Project 1";
-    document.body.children[1].children[0].children[1].children[2].children[0].innerHTML = "Project 2";
+    if (lang.includes("de")){
+        document.body.children[1].children[0].children[0].children[1].innerHTML = german["project_overview"];
+        document.body.children[1].children[0].children[0].children[2].innerHTML = german["new_project"];
+        document.body.children[1].children[0].children[1].children[0].innerHTML = german["content"];
+        document.body.children[1].children[0].children[1].children[1].children[0].innerHTML = german["project"];
+        document.body.children[1].children[0].children[1].children[2].children[0].innerHTML = german["project"];
+
+    }
+    else if (lang.includes("en")){
+        document.body.children[1].children[0].children[0].children[1].innerHTML = english["project_overview"];
+        document.body.children[1].children[0].children[0].children[2].innerHTML = english["new_project"];
+        document.body.children[1].children[0].children[1].children[0].innerHTML = english["content"];
+        document.body.children[1].children[0].children[1].children[1].children[0].innerHTML = english["project"];
+        document.body.children[1].children[0].children[1].children[2].children[0].innerHTML = english["project"];
+
+    }
+
+    for (k=0; k<project_list.length; k++){
+        let article= (title + project_list[k].titel + body +project_list[k].kurzbeschreibung + link + "Projekt1.html" + end);
+
+        document.body.children[1].children[0].children[2].innerHTML += article;
+    }
 
 }
